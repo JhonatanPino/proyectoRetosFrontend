9ea128ad5dcd88341c4c48926ceb590a297eab88
JhonatanPino <jhonatanpinorivera@gmail.com>
2025-10-30 01:49:34 -0500

    modify base_url_api

--- src/Config.jsx
+++ src/Config.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 
-const base_api_url = "http://localhost:8000/api";
+const base_api_url = "http://proyectoretos-production.up.railway.app/api";
 
 export default{
     //PUBLIC
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
         port: 5173,
         proxy: {
             '/api': {
-                target: 'http://127.0.0.1:8000', // Si necesitas comunicarte con un backend Laravel
+                target: 'http://proyectoretos-production.up.railway.app', // Si necesitas comunicarte con un backend Laravel
                 changeOrigin: true,
                 secure: false,
             },
